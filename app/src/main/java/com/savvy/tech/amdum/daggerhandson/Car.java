85fc2dff3403a73df12f0d9bc00303385111c5db
swapna <globalfriends747@gmail.com>
2019-03-21 16:17:30 -0400

    First Version

---
+++ app/src/main/java/com/savvy/tech/amdum/daggerhandson/Car.java
@@ -0,0 +1,30 @@
+package com.savvy.tech.amdum.daggerhandson;
+
+import android.util.Log;
+
+import javax.inject.Inject;
+
+public class Car {
+    private static final String TAG = "Car";
+
+    private Engine engine;
+    private Wheels wheels;
+    private Audio audio;
+
+    @Inject
+    public Car(Engine engine, Wheels wheels, Audio audio) {
+        this.engine = engine;
+        this.wheels = wheels;
+        this.audio = audio;
+
+    }
+
+    @Inject
+    public void enableRemote(Remote remote) {
+        remote.setListener(this);
+    }
+
+    public void drive(){
+        Log.d(TAG, "drive: ");
+    }
+}
